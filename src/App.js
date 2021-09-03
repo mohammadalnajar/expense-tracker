154c8ffea0a7756847efe3065ecd6168f50f8f53
Mohammad <malnajjar16@gmail.com>
2021-09-03 11:43:50 +0200

    adding header+balance+incomeexpenses components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
 
 function App() {
-  return <div>hello World</div>;
+  return (
+    <div>
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+      </div>
+    </div>
+  );
 }
 
 export default App;
